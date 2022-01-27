0b6c65a83978a9b7736be91ef990db4cbf66a44e
vivekanandhansj <97933338+vivekanandhansj@users.noreply.github.com>
2022-01-27 10:50:11 +0000

    1. For the given JSON iterate over all for loops (for, for in, for of, forEach)

---
+++ script.js
@@ -0,0 +1,26 @@
+// it defines a number
+// it takes a integer value
+console.log (typeof(1));
+// it defines a number
+// it takes float number
+console.log  (typeof(1.1));
+// it defines a string
+// which is always enclosed with a single ('') or double quotes (“ ”)
+console.log  (typeof('1.1'));
+// it defines a boolean
+// it has true and false
+console.log (typeof(true));
+// it defines a object
+// null is an object where it is primitive
+console.log (typeof(null));
+// it defines a undefined
+//Whenever a function in JavaScript does not explicitly return anything, it returns undefined by default
+console.log (typeof(undefined));
+// it defines object
+// it takes empty square bracket is a object
+console.log (typeof([]));
+// it defines object
+// it takes curly bracket is a object
+console.log (typeof({}));
+// it defines number
+console.log (typeof(NaN));
