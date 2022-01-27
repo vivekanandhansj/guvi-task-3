45a2c31a3a0511597ba588ec6b2495dd5bf4f617
vivekanandhansj <97933338+vivekanandhansj@users.noreply.github.com>
2022-01-27 10:59:36 +0000

    2. Create your own resume data in JSON format

---
+++ resume.js
@@ -0,0 +1,10 @@
+var resume=[{"Name":"Vivekanandhan.J",
+             "Qualification":"B.E",
+             "Email":"vivekmech249@gmail.com",
+            "Experience":"Fresher",
+        "Address":"107, Poosari valasu, Polavakalipalayam, Gobi, Erode",
+    "Domain":"Full Stack Development",}];
+ //console.log(resume);
+resume.forEach(function() {
+    console.log(resume);
+});
